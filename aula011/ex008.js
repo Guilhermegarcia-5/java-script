18d09aff79eb68373a3902e892dc57498034fda7
Guilherme Garcia da Silveira <gui7garcia7gui@gmail.com>
2024-11-07 08:18:58 -0300

    ex010

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { // CONDIÇÃO SIMPLES
+    console.log('MULTADO!')
+}
+console.log(`dirija sempre usando cinto de segurança`)
