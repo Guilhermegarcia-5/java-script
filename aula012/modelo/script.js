d4755c67dc768efd72585c87942e007dc8d4947b
Guilherme Garcia da Silveira <gui7garcia7gui@gmail.com>
2024-11-11 10:06:57 -0300

    ex016

---
+++ aula012/modelo/script.js
@@ -0,0 +1,35 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] faltam dados!')
+        res.innerHTML = 'impossivel contar!'
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('passo invalido')
+            p = 1
+        }
+
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `${c} 🏁`
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `${c} 🏁`
+        }
+
+    }
+}
